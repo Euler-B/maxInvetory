f667ab8b2797a5a76d8cc7e7fafc667509ab6c2f
Eduardo Bravo <eduardojosebb.matescience@gmail.com>
2024-02-10 08:33:51 -0400

    add db and docker container strategies

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/euler-b/maxInventoryProject
 go 1.21.6
 
 require (
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/jmoiron/sqlx v1.3.5 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
 	go.uber.org/dig v1.17.1 // indirect
 	go.uber.org/fx v1.20.1 // indirect
--- main.go
+++ main.go
@@ -1,25 +1,33 @@
 package main
 
 import (
-	"log"
+	"context"
 
+	"github.com/euler-b/maxInventoryProject/database"
 	"github.com/euler-b/maxInventoryProject/settings"
+	"github.com/jmoiron/sqlx"
+
 	"go.uber.org/fx"
 )
 
 func main() {
 	app := fx.New(
 		fx.Provide(
+			context.Background,
 			settings.New,
+			database.New,
 		),
 
 		fx.Invoke(
-			func(s *settings.Settings) {
-				log.Println(s)
+			func(db *sqlx.DB) {
+				_, err := db.Query("select * from USERS")
+				if err != nil {
+					panic(err)
+				}
 			},
 		),
 	)
-app.Run()
+	app.Run()
 
-// Dejo este codigo presente en este commit para saber si el app en cuestion esta funcionando.
+	// Dejo este codigo presente en este commit para saber si el app en cuestion esta funcionando.
 }
