33163bffea92769db38dd5f8812a4d0f6ca4c254
Eduardo Bravo <eduardojosebb.matescience@gmail.com>
2024-02-16 17:53:35 -0400

    feat: :sparkles: Added jwt token and cookies for security session in the API.

--- internal/api/dtos/login_user.go
+++ internal/api/dtos/login_user.go
@@ -1,9 +1,6 @@
 package dtos
 
-type RegisterUser struct {
+type LoginUser struct {
 	Email    string `json:"email" validate:"required,email"`
 	Password string `json:"password" validate:"required,min=8"`
-	Name     string `json:"name" validate:"required"`
 }
-
-
